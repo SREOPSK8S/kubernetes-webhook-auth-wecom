d3cbc74237c861a8d947a085ff27574c6be4d674
阳之幸福 <1689484241@qq.com>
2022-04-10 08:44:25 +0800

    Sun Apr 10 08:44:05 CST 2022 init project and create go mod

---
+++ entity/wecom.go
@@ -0,0 +1,19 @@
+package entity
+
+type CorpIDAndSecret struct {
+	CorpID     string
+	CorpSecret string
+}
+
+type AccessTokenResponse struct {
+	ErrorCode    int    `json:"errcode"`
+	ErrorMessage string `json:"errmsg"`
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+}
+
+func (ac *AccessTokenResponse) GetAccessToken() (accessToken string) {
+	accessToken = ac.AccessToken
+	return
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SREOPSK8S/kubernetes-webhook-auth-wecom
+
+go 1.17
