06a2321260cf0e82e259740e38619e527ca3e00a
阳之幸福 <1689484241@qq.com>
2022-04-10 09:19:03 +0800

    Sun Apr 10 09:18:48 CST 2022 Add config file

--- config/config.go
+++ config/config.go
@@ -1,8 +1,41 @@
 package config
 
 import (
-	_ "github.com/spf13/viper"
+	"github.com/spf13/viper"
+	"os"
 )
+
 func InitAndLoad() {
-	viper.SetConfigName("config")
+	viper.SetConfigName("auth-wecom")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./conf")
+	viper.AddConfigPath("../")
+	if err := viper.ReadInConfig(); err != nil {
+		panic("load config file error")
+	}
+}
+
+func GetServerPort() string {
+	return viper.GetString("server.port")
+}
+
+func GetCorpID() (corpID string) {
+	corpIDEnv := os.Getenv("CORP_ID")
+	if corpIDEnv != "" {
+		corpID = corpIDEnv
+		return
+	}
+	corpID = viper.GetString("WeCom.CorpID")
+	return
+}
+
+func GetCorpSecret() (corpSecret string ){
+	corpSecretEnv :=  os.Getenv("CORP_SECRET")
+	if corpSecretEnv != "" {
+		corpSecret = corpSecretEnv
+		return
+	}
+	corpSecret = viper.GetString("WeCom.CorpSecret")
+	return
 }
