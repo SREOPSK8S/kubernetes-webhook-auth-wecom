4cd8a19191a97e9d9e3129e4b4600ef5b08e8177
阳之幸福 <1689484241@qq.com>
2022-04-12 21:48:48 +0800

    Tue Apr 12 21:48:18 CST 2022 update wecom file add  ReadMemberResponse and GetDepartmentDetailsResponse struct

--- entity/wecom/wecom.go
+++ entity/wecom/wecom.go
@@ -5,11 +5,40 @@ type CorpIDAndSecret struct {
 	CorpSecret string
 }
 
-type AccessTokenResponse struct {
+type BaseResponse struct {
 	ErrorCode    int    `json:"errcode"`
 	ErrorMessage string `json:"errmsg"`
+}
+type AccessTokenResponse struct {
+	BaseResponse
 	AccessToken  string `json:"access_token"`
 	ExpiresIn    int    `json:"expires_in"`
+}
+// ReadMemberResponse https://developer.work.weixin.qq.com/document/path/90196
+type ReadMemberResponse struct {
+	BaseResponse
+	Userid string `json:"userid"`
+	Name string `json:"name"`
+	Department []int `json:"department"`
+	Mobile string `json:"mobile"`
+	Gender string `json:"gender"`
+	Email string `json:"email"`
+	Telephone string `json:"telephone"`
+	Alias string `json:"alias"`
+	Status int `json:"status"`
+}
+// GetDepartmentDetailsResponse https://developer.work.weixin.qq.com/document/path/95351
+type GetDepartmentDetailsResponse struct {
+	BaseResponse
+	Department Department `json:"department"`
+}
+type Department struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	NameEn string `json:"name_en"`
+	DepartmentLeader []string `json:"department_leader"`
+	ParentID int `json:"parentid"`
+	Order int `json:"order"`
 }
 
 func (ac *AccessTokenResponse) GetAccessToken() (accessToken string) {
