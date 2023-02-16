e52ca344a74b4d6f0b648d7db876142c6bb84a78
阳之幸福 <1689484241@qq.com>
2023-02-16 19:33:04 +0800

    update

--- cmd/auth-server/main.go
+++ cmd/auth-server/main.go
@@ -1,8 +1,12 @@
 package main
 
-import  "github.com/SREOPSK8S/kubernetes-webhook-auth-wecom/interfaces/router"
+import (
+	"github.com/SREOPSK8S/kubernetes-webhook-auth-wecom/config"
+	"github.com/SREOPSK8S/kubernetes-webhook-auth-wecom/interfaces/router"
+)
 
 func main() {
+	config.InitAndLoad()
 	router := router.GetRouter()
 	router.Run(":8443")
 }
